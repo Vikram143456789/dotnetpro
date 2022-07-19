358d95c630d4a68817ee438363d64386eb2d96a3
Vikram143456789 <lokeshtk2000@gmail.com>
2022-07-19 20:35:55 +0530

    initial commit

---
+++ HelloWordAPP.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace HelloWordAPP.Web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
